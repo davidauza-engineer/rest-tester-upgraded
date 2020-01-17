4e1c398dd23a878d139d7fcc1a601bf8e3579bc1
davidauza.engineer <davidauza01@gmail.com>
2020-01-17 15:30:59 -0500

    Update rest_requestor.rb

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -5,17 +5,29 @@ def request_integer_input
   gets.chomp.to_i
 end
 
-url = 'http://localhost:3000/users'
+def print_options(url_array)
+  url_array.each_index do |index|
+    next if index == 0
+    puts "Press #{index} to execute a GET request to #{url_array[index]}"
+    puts "\n" if index == (url_array.count - 1)
+  end
+end
 
-options = "Press 1 to execute a GET request to #{url}"
-puts "\nWelcome\n\n#{options}\n\n"
+url_array = %w['' http://localhost:3000/users http://localhost:3000/users/1
+http://localhost:3000/users/new http://localhost:3000/users/1/edit]
+
+puts "\nWelcome\n\n"
+
+print_options(url_array)
+
 user_input = request_integer_input
 
-while user_input != 1
-  puts "\nNot a valid entry. \n\n#{options}\n\n"
+until user_input.between?(1, url_array.length - 1)
+  puts "\nNot a valid entry.\n\n"
+  print_options(url_array)
   user_input = request_integer_input
 end
 
 puts
-puts RestClient.get(url)
+puts RestClient.get(url_array[user_input])
 puts
